6154dc53d83c17d9829343338d40008aae4febbf
Trevor Mudarikwa <64070031+Nymud@users.noreply.github.com>
2021-03-21 23:07:25 +0200

    added files via upload

---
+++ game_show_app_v1.2/appp.js
@@ -0,0 +1,188 @@
+//create variables
+
+const qwerty = document.getElementById('qwerty');
+const phrase= document. querySelector('#phrase ul');
+let   miss = 0;
+let tries = 0;
+let highestMissed = 5;
+const ul = document.querySelector('#phrase ul')
+
+//const tries=document.getElementsByTagName('tries')[i];
+
+
+
+
+const startGame = document.querySelector('a.btn__reset');
+const overlay = document.getElementById('overlay');
+let phrases= ['java' , 'php' , 'ruby' , 'python' , 'react'];
+let scoreboard = document.getElementById('scoreboard');
+const mainHead = document.querySelector('.title');
+
+
+
+
+//addEvent listener
+
+startGame.addEventListener('click', function() {
+  let letters= document.getElementsByClassName("letter");
+  let space=document.getElementsByClassName("space");
+  for ( let i=letters.length-1 ; i>=0 ;  i--){
+    if (letters===""){
+
+    letters.parentNode.removeChild(letters[i]);
+  }
+
+
+
+  overlay.style.display='none';
+
+}
+
+});
+
+
+
+//get randomphrase array and use split methode on it.
+function getRandomPhrasesArray(arr){
+  var PhrasesArray= arr[(Math.floor(Math.random() * arr.length))];
+  var NewPhrasesArray= PhrasesArray.split();
+  return NewPhrasesArray;
+}
+
+
+
+//create addPhraseToDisplay function
+
+function  addPhrasesDisplay(arr){
+
+
+let array=arr[0].split('');
+console.log(array);
+
+let win=arr
+
+      for ( i=0; i< array.length; i++){
+
+        let list =document.createElement('li');
+        list.textContent=array[i];
+        phrase.appendChild(list);
+        //list.appendChild(text);
+
+
+    if(array[i]===' '){
+      list.className='space';
+    }else {
+      list.className='letter';
+    };
+  };
+};
+
+
+
+  addPhrasesDisplay(getRandomPhrasesArray(phrases));
+
+
+
+qwerty.addEventListener('click',function(e){
+  if (e.target .tagName==='BUTTON'){
+    const target= e.target;
+    const button=target;
+    button.className += "chosen";
+    button.setAttribute('disabled',true);
+    const foundLetter= checkLetter(target);
+
+
+
+
+  if (foundLetter===null){
+      miss ++;
+      if(miss >=1 && miss <= highestMissed){
+        let lives=document.querySelector('.tries').firstChild;
+        lives.src='./images/lostHeart.png';
+        lives.parentElement.classList.add('tried');
+        lives.parentElement.classList.remove('tries');
+
+
+      }
+
+
+    };
+  checkWin();
+}
+  });
+
+
+
+//letter appears in a phrase
+
+function checkLetter(target){
+  let foundLetter= null;
+
+
+  const letter=document.getElementsByClassName('letter');
+  const btn=target.textContent;
+  for ( let i=0; i<letter.length; i++){
+    //if matches found
+    if(letter[i].textContent.toLowerCase()==target.textContent.toLowerCase()){
+      letter[i].classList.add('show');
+      foundLetter=letter[i];
+
+
+    }
+
+}
+  return foundLetter;
+};
+
+//function used with the checkWin function
+
+
+function checkWin( ){
+  //append winText and loseText to the mainHead
+
+  let letterClassShow=document.querySelectorAll(".letter");
+  let letterClassLetter=document.querySelectorAll('.show');
+
+
+  if (letterClassShow.length===letterClassLetter.length ){
+   overlay.style.display= "flex";
+   overlay.className="win";
+   mainHead.innerHTML = "YOU WIN!";
+   appReset();
+
+  }
+  else if (miss===5){
+
+  overlay.className="lose";
+  mainHead.innerHTML = "YOU LOSE!";
+  overlay.style.display="flex";
+  appReset();
+}
+
+}
+
+
+function appReset() {
+  let button = document.createElement('button');
+  button.innerHTML = "Reset";
+  button.id = "reset";
+  button.classList.add('btn__reset');
+  overlay.append(button);
+
+  button.addEventListener('click',() => { miss = 0;
+    addPhrasesDisplay(getRandomPhrasesArray(phrases));
+    const buttonReset = document.getElementsByTagName('button');
+    const correctButtons = document.querySelectorAll('.letter.show');
+    for( let i = 0; i < correctButtons.length; i++){
+      correctButtons[i].classList.remove("show");
+      correctButtons[i].style.display = "none";
+    }
+    for(let i = 0; i < buttonReset.length; i ++) {
+      buttonReset[i].classList.remove("chosen"); 
+      buttonReset[i].setAttribute('disabled',false);
+   }
+   window.alert('Game has been reset! Click Start to play again.');
+  }
+  );
+  
+}
